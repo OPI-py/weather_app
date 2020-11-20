d76a8baeffe50b0b26f53b3aa8f01948c07cef77
OPI-py <oleksandrp09@gmail.com>
2020-11-20 19:39:55 +0200

    Adjust code

--- main.py
+++ main.py
@@ -15,90 +15,72 @@ class WeatherApp(QMainWindow):
 		self.ui()
 
 	def ui(self):
-		self.label = QtWidgets.QLabel(self)
-		self.label.setText("Country")
+		self.label = QtWidgets.QLabel("Country", self)
 		self.label.move(100, 150)
 
-		self.label_1 = QtWidgets.QLabel(self)
-		self.label_1.setText("Country:")
+		self.label_1 = QtWidgets.QLabel("Country:", self)
 		self.label_1.move(30, 150)
 
 
-		self.label2 = QtWidgets.QLabel(self)
-		self.label2.setText("City")
+		self.label2 = QtWidgets.QLabel("City", self)
 		self.label2.move(100, 200)
 
-		self.label2_1 = QtWidgets.QLabel(self)
-		self.label2_1.setText("City:")
+		self.label2_1 = QtWidgets.QLabel("City:", self)
 		self.label2_1.move(30, 200)
 
 
-		self.label3 = QtWidgets.QLabel(self)
-		self.label3.setText("Temperature")
+		self.label3 = QtWidgets.QLabel("Temperature", self)
 		self.label3.move(100, 250)
 
-		self.label3_1 = QtWidgets.QLabel(self)
-		self.label3_1.setText("Temperature:")
+		self.label3_1 = QtWidgets.QLabel("Temperature:", self)
 		self.label3_1.move(30, 250)
 
 
-		self.label4 = QtWidgets.QLabel(self)
-		self.label4.setText("Feels like")
+		self.label4 = QtWidgets.QLabel("Feels like", self)
 		self.label4.move(100, 300)
 
-		self.label4_1 = QtWidgets.QLabel(self)
-		self.label4_1.setText("Feels like:")
+		self.label4_1 = QtWidgets.QLabel("Feels like:", self)
 		self.label4_1.move(30, 300)
 
 
-		self.label5 = QtWidgets.QLabel(self)
-		self.label5.setText("Clouds")
+		self.label5 = QtWidgets.QLabel("Clouds", self)
 		self.label5.move(100, 350)
 
-		self.label5_1 = QtWidgets.QLabel(self)
-		self.label5_1.setText("Clouds:")
+		self.label5_1 = QtWidgets.QLabel("Clouds:", self)
 		self.label5_1.move(30, 350)
 
 
-		self.label6 = QtWidgets.QLabel(self)
-		self.label6.setText("Pressure")
+		self.label6 = QtWidgets.QLabel("Pressure", self)
 		self.label6.move(300, 150)
 
-		self.label6_1 = QtWidgets.QLabel(self)
-		self.label6_1.setText("Pressure:")
+		self.label6_1 = QtWidgets.QLabel("Pressure:", self)
 		self.label6_1.move(230, 150)
 
 
-		self.label7 = QtWidgets.QLabel(self)
-		self.label7.setText("Humidity")
+		self.label7 = QtWidgets.QLabel("Humidity", self)
 		self.label7.move(300, 200)
 
-		self.label7_1 = QtWidgets.QLabel(self)
-		self.label7_1.setText("Humidity:")
+		self.label7_1 = QtWidgets.QLabel("Humidity:", self)
 		self.label7_1.move(230, 200)
 
 
-		self.label8 = QtWidgets.QLabel(self)
-		self.label8.setText("Longitude")
+		self.label8 = QtWidgets.QLabel("Longitude", self)
 		self.label8.move(300, 300)
 
-		self.label8_1 = QtWidgets.QLabel(self)
-		self.label8_1.setText("Longitude:")
+		self.label8_1 = QtWidgets.QLabel("Longitude:", self)
 		self.label8_1.move(230, 300)
 
 
-		self.label9 = QtWidgets.QLabel(self)
-		self.label9.setText("Latitude")
+		self.label9 = QtWidgets.QLabel("Latitude", self)
 		self.label9.move(300, 250)
 
-		self.label9_1 = QtWidgets.QLabel(self)
-		self.label9_1.setText("Latitude:")
+		self.label9_1 = QtWidgets.QLabel("Latitude:", self)
 		self.label9_1.move(230, 250)
 
 
 		self.b1 = QtWidgets.QPushButton("Find", self)
 		self.b1.clicked.connect(self.clicked)
-		self.b1.resize(60, 29)
+		self.b1.resize(80, 29)
 		self.b1.move(50, 50)
 
 		self.b2 = QtWidgets.QPushButton("Exit", self)
@@ -126,7 +108,7 @@ class WeatherApp(QMainWindow):
 
 	def weather(self):
 		api_key = ""
-       # API key can be register at https://openweathermap.org/
+       # API key can be register on https://openweathermap.org/
 		city = self.city_edit.text()
 
 		w_result = requests.get("https://api.openweathermap.org/data/2.5/" + 
